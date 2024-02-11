26d61fd785b04b275508007de504bbdcac2917af
Muhammad_Asad <156535903+256Asad@users.noreply.github.com>
2024-02-11 18:02:13 +0500

    Update simple_calculator.py

--- simple_calculator.py.py
+++ simple_calculator.py.py
@@ -1,4 +1,3 @@
-
 # ........................................MUHAMMAD ASAD AZHAR...................................................
 # .....................................Gmail  asadm2839@gmail.com................................................
 # .........................................STUDENT ID : 62684...................................................
@@ -22,44 +21,65 @@ def multiply(a , b):
 def divide(a , b):
     return a / b
 
+while True:
+
+    # list of functions to use it
+    print("1. adiition") 
+    print("2. subtraction") 
+    print("3. multiplication") 
+    print("4. Division") 
+    print("5. Exit") 
+
+    # selected function to use 
+
+    select = input("Enter choice 1 : 2 : 3 : 4 : 5 :-  ")
+
+    # condition check
+    if select == '1':
+        # user input
+
+        num1 = int(input("Enter first number:- "))
+        num2 = int(input("Enter second number:- "))
+        result = num1 + num2
+        print('.........................................................')
+        print(f"             Answer :- {num1} + {num2} = {result}")
+        print('.........................................................')
+
+    elif select == '2':
+        num1 = int(input("Enter first number:- "))
+        num2 = int(input("Enter second number:- "))
+        result = num1 - num2
+        print('.........................................................')
+        print(f"             Answer :- {num1} - {num2} = {result}")
+        print('.........................................................')
 
 
-# list of functions to use it
-print("1. adiition") 
-print("2. subtraction") 
-print("3. multiplication") 
-print("4. Division") 
-print("5. Exit") 
-
-# selected function to use 
-
-select = input("Enter choice 1 : 2 : 3 : 4 : ")
+    elif select == '3':
+        num1 = int(input("Enter first number:- "))
+        num2 = int(input("Enter second number:- "))
+        result = num1 * num2
+        print('.........................................................')
+        print(f"             Answer :- {num1} * {num2} = {result}")
+        print('.........................................................')
 
 
-if select == '1':
-    num1 = int(input("Enter first number: "))
-    num2 = int(input("Enter second number: "))
-    result = num1 + num2
-    print(f"{num1} + {num2} = {result}")
-
-elif select == '2':
-    num1 = int(input("Enter first number: "))
-    num2 = int(input("Enter second number: "))
-    result = num1 - num2
-    print(f"{num1} - {num2} = {result}")
-
-elif select == '3':
-    num1 = int(input("Enter first number: "))
-    num2 = int(input("Enter second number: "))
-    result = num1 * num2
-    print(f"{num1} * {num2} = {result}")
-
-elif select == '4':
-    num1 = int(input("Enter first number: "))
-    num2 = int(input("Enter second number: "))
-    result = num1 / num2
-    print(f"{num1} / {num2} = {result}")
-
-
+    elif select == '4':
+        num1 = int(input("Enter first number:- "))
+        num2 = int(input("Enter second number:- "))
+        result = num1 / num2
+        print('.........................................................')
+        print(f"             Answer :- {num1} / {num2} = {result}")
+        print('.........................................................')
 
     
+    elif select == '5':
+        print('.........................................................')
+        print(f"             Exiting The Calculator . Goodbye :-   ")
+        print('.........................................................')
+        break
+
+    else :
+        print('.........................................................')
+        print(f"             Your selection is invalit                  ")
+        print(f"                 Please Try again ...                 ")
+        print('.........................................................')
